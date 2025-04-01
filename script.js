c2563fc12f7d3bfd99155536f6009b72edbca93e
Rodd Azad <roddazad@Rodds-MacBook-Pro.local>
2025-04-01 13:26:27 -0700

    coded the function to create elements for content, card, edit and delete actions

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener("load", loadNotes);
 // EVENT: Add Note Button Clicked
 // ===============================
 addNoteBtn.addEventListener("click", () => {
-    const noteText = noteInput.ariaValueMax.trim();
+    const noteText = noteInput.value.trim();
     // Check if note input is not empty
     if (noteText !== "") {
         // Save note to local storage
@@ -42,4 +42,41 @@ function saveNotes(note) {
 function getNotesFromStorage() {
     const  notes = localStorage.getItem("notes");
     return notes ? JSON.parse(notes) : [];
+};
+
+// ===============================
+// FUNCTION: Load and Display Notes
+// ===============================
+function loadNotes() {
+    // clear current note in the DOM noteContainer
+    notesContainer.innerHTML = "";
+    const notes = getNotesFromStorage();
+     // Loop through each note and create a card
+     notes.forEach((notes, index) => {
+        const noteCards = document.createElement("div");
+        noteCards.classList.add("note-card")
+
+        //note Content
+        const noteText = document.createElement("div");
+        noteText.classList.add("note-text");
+
+        // Note actions: Edit & Delete
+         const actions = document.createElement("div");
+         actions.classList.add("note-actions");
+             // Edit Button
+             const editBtn = document.createElement("button");
+             editBtn.textContent = "Edit";
+             editBtn.classList.add("edit-btn");
+             editBtn.addEventListener("click", () => editNote(index));
+             // Delete Button
+             const deleteBtn = document.createElement("button");
+             deleteBtn.textContent = "Delete";
+             deleteBtn.classList.add("delete-btn");
+             deleteBtn.addEventListener("click", () => deleteNote(index));
+        
+        // Append the edit and delete buttons to the action div
+        
+
+     })
+
 };
